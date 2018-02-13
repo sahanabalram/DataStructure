ad07b59f985363abab9558b52738af630fbac28e
Sahana Balram <sahana2008balram@gmail.com>
2018-02-13 15:47:17 -0800

    Add delete node code

--- doublyLinkedList.js
+++ doublyLinkedList.js
@@ -1,3 +1,6 @@
+/* What is Doubly Linked list
+A Doubly Linked List is a linked data structure that consists of a set of sequentially linked records called nodes. Each node contains two fields, called links, that are references to the previous and to the next node in the sequence of nodes */
+
 let DoublyLinkedList = function () {
     this.length = 0;
     this.head = null;
@@ -7,18 +10,61 @@ let DoublyLinkedList = function () {
 let Node = function (value) {
     this.value = value;
     this.next = null;
+    this.previous = null;
 }
 
-DoublyLinkedList.prototype.insertFront = function(value) {
+DoublyLinkedList.prototype.printAll = function() {
+    console.log(this);
+}
+DoublyLinkedList.prototype.print = function () {
+    temp = this.head;
+    while (temp != null) {
+        console.log(temp.value);
+        temp = temp.next;
+    }
+}
+DoublyLinkedList.prototype.insertFront = function (value) {
     let node = new Node(value);
 
-    if(!this.head === null) {
+    if (!this.head) {
         this.head = node;
-        this.tail =node;
+        this.tail = node;
     } else {
         node.previous = this.tail;
         this.tail.next = node;
         this.tail = node;
     }
     this.length++;
-}
+};
+
+DoublyLinkedList.prototype.delete = function (value) {
+    let currentNode = this.head;
+    while (currentNode) {
+        if (currentNode.value === value) {
+            if (currentNode === this.head && currentNode === this.tail) {
+                this.head = null;
+                this.tail = null;
+            } else if (currentNode === this.head) {
+                this.head = this.head.next;
+                this.head.previous = null;
+            } else if (currentNode === this.tail) {
+                this.tail = this.tail.previous;
+                this.tail.next = null;
+            } else {
+                currentNode.previous.next = currentNode.next;
+                currentNode.next.previous = current.previous;
+            }
+            this.length--;
+        }
+        currentNode = currentNode.next;
+    }
+};
+head = new DoublyLinkedList();
+head.insertFront(20);
+head.insertFront(21);
+head.insertFront(22);
+head.insertFront(16);
+head.print();
+head.delete(20);
+head.print();
+head.printAll();
