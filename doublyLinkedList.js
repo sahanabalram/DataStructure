d11e3bf9ccbd14b18dfeb921ddd74fb1075fd82a
Sahana Balram <sahana2008balram@gmail.com>
2018-02-13 15:34:24 -0800

    Insert Front code

--- doublyLinkedList.js
+++ doublyLinkedList.js
@@ -1,6 +1,7 @@
 let DoublyLinkedList = function () {
     this.length = 0;
     this.head = null;
+    this.tail = null;
 }
 
 let Node = function (value) {
@@ -8,3 +9,16 @@ let Node = function (value) {
     this.next = null;
 }
 
+DoublyLinkedList.prototype.insertFront = function(value) {
+    let node = new Node(value);
+
+    if(!this.head === null) {
+        this.head = node;
+        this.tail =node;
+    } else {
+        node.previous = this.tail;
+        this.tail.next = node;
+        this.tail = node;
+    }
+    this.length++;
+}
